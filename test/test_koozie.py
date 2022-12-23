bf3e49bdea9057a1eb3a86bc10b498888f38606d
Neal Kruis <neal.kruis@bigladdersoftware.com>
2022-12-22 18:38:40 -0700

    Clean up CLI test.

--- test/test_koozie.py
+++ test/test_koozie.py
@@ -12,23 +12,26 @@ def test_units():
   assert to_u(0.0254,"in") == approx(1.0)
   assert fr_u(3.41241633,"Btu/h") == approx(1.0,0.0001)
 
+runner = CliRunner()
+
+def cli_wrapper(args=[], expect_failure=False):
+  result = runner.invoke(koozie_cli, args)
+  assert (result.exit_code != 0) == expect_failure
+  return result.output
+
 def test_cli():
-  runner = CliRunner()
-  result = runner.invoke(koozie_cli, ["--help"])
-  assert result.exit_code == 0
-  result = runner.invoke(koozie_cli, ["-l"])
-  assert result.exit_code == 0
-  result = runner.invoke(koozie_cli, ["-40", "degF", "degC"])
-  assert result.exit_code == 0
-  output = result.output
+  cli_wrapper(["--help"])
+  cli_wrapper(["-l"])
+
+  output = cli_wrapper(["-40", "degF", "degC"])
   assert round(float(output[:8])) == -40.
   assert output[-3:-1] == "°C"
-  result = runner.invoke(koozie_cli, ["1", "in"])
-  assert result.exit_code == 0
-  assert float(result.output.split(' ')[0]) == 0.0254
-  result = runner.invoke(koozie_cli, ["1", "in", "day"])
-  assert result.exit_code != 0
-  assert "Cannot convert from 'inch' ([length]) to 'day' ([time])" in result.output
-  result = runner.invoke(koozie_cli, ["1", "cubit"])
-  assert result.exit_code != 0
-  assert "'cubit' is not defined in the unit registry" in result.output
+
+  output = cli_wrapper(["1", "in"])
+  assert float(output.split(' ')[0]) == 0.0254
+
+  output = cli_wrapper(["1", "in", "day"],True)
+  assert "Cannot convert from 'inch' ([length]) to 'day' ([time])" in output
+
+  output = cli_wrapper(["1", "cubit"],True)
+  assert "'cubit' is not defined in the unit registry" in output
