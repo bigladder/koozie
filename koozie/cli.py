f0a425f2bca09526a75711d7435624bce5d519ce
Tanaya Mankad <tanaya.mankad@bigladdersoftware.com>
2023-04-03 12:42:34 -0600

    Apply black for in-place formatting and pylint for style/error linting.

--- koozie/cli.py
+++ koozie/cli.py
@@ -1,48 +1,68 @@
-import koozie
-import click
+"""Command-line interface for koozie"""
+
 import sys
+import click
+import koozie
+
 
 def list_callback(ctx: click.Context, param: click.Parameter, value):
-  if not value or ctx.resilient_parsing:
-      return
+    """"""
+    if not value or ctx.resilient_parsing:
+        return
 
-  unit_list = koozie.koozie.get_unit_list()
+    unit_list = koozie.koozie.get_unit_list()
 
-  for dim in unit_list:
-    dim_aliases = ""
-    if len(unit_list[dim]["aliases"]) > 0:
-      dim_aliases = f" ({', '.join(unit_list[dim]['aliases'])})"
-    if value in dim or value == "*" or value in dim_aliases:
-      heading = f"{dim}{dim_aliases}"
-      click.echo(heading)
-      click.echo("-"*len(heading))
-      for unit in unit_list[dim]["units"]:
-        aliases = ""
-        if len(unit_list[dim]["units"][unit]) > 0:
-          aliases = f" ({', '.join(unit_list[dim]['units'][unit])})"
-        try:
-          click.echo(f"  - {unit}{aliases}")
-        except UnicodeEncodeError as e:
-          click.echo(f"  - {unit}")
-      click.echo(f"\n")
+    for dim in unit_list:
+        dim_aliases = ""
+        if len(unit_list[dim]["aliases"]) > 0:
+            dim_aliases = f" ({', '.join(unit_list[dim]['aliases'])})"
+        if value in dim or value == "*" or value in dim_aliases:
+            heading = f"{dim}{dim_aliases}"
+            click.echo(heading)
+            click.echo("-" * len(heading))
+            for unit in unit_list[dim]["units"]:
+                aliases = ""
+                if len(unit_list[dim]["units"][unit]) > 0:
+                    aliases = f" ({', '.join(unit_list[dim]['units'][unit])})"
+                try:
+                    click.echo(f"  - {unit}{aliases}")
+                except UnicodeEncodeError:
+                    click.echo(f"  - {unit}")
+            click.echo("\n")
 
-  ctx.exit()
+    ctx.exit()
 
-@click.command(context_settings=dict(help_option_names=['-h', '--help'], ignore_unknown_options=True))
-@click.version_option(None,'-v','--version')
-@click.option('-l', '--list', help='Print a list of available units by dimension (e.g., "power"). Default: list all units.', type=click.STRING, is_flag=False, flag_value="*", callback=list_callback)
-@click.argument('value', type=click.FLOAT)
-@click.argument('from_units', type=click.STRING)
-@click.argument('to_units', type=click.STRING, required=False)
-def koozie_cli(value, from_units, to_units, list):
-  """koozie: Convert VALUE from FROM_UNITS to TO_UNITS.
 
-  If TO_UNITS is not specified, VALUE will be converted from FROM_UNITS into base SI units.
-  """
-  try:
-    if to_units is None:
-      click.echo(f"{koozie.koozie.fr_q(value,from_units)}")
-    else:
-      click.echo(f"{koozie.koozie.convert_q(value,from_units,to_units)}")
-  except (koozie.koozie.pint.UndefinedUnitError, koozie.koozie.pint.DimensionalityError) as e:
-    sys.exit(e)
+@click.command(
+    context_settings=dict(
+        help_option_names=["-h", "--help"], ignore_unknown_options=True
+    )
+)
+@click.version_option(None, "-v", "--version")
+@click.option(
+    "-l",
+    "--list",
+    help='Print a list of available units by dimension (e.g., "power"). Default: list all units.',
+    type=click.STRING,
+    is_flag=False,
+    flag_value="*",
+    callback=list_callback,
+)
+@click.argument("value", type=click.FLOAT)
+@click.argument("from_units", type=click.STRING)
+@click.argument("to_units", type=click.STRING, required=False)
+def koozie_cli(value, from_units, to_units):
+    """koozie: Convert VALUE from FROM_UNITS to TO_UNITS.
+
+    If TO_UNITS is not specified, VALUE will be converted from FROM_UNITS into base SI units.
+    """
+    try:
+        if to_units is None:
+            click.echo(f"{koozie.koozie.fr_q(value,from_units)}")
+        else:
+            click.echo(f"{koozie.koozie.convert_q(value,from_units,to_units)}")
+    except (  # pylint: disable=C0103
+        koozie.koozie.pint.UndefinedUnitError,
+        koozie.koozie.pint.DimensionalityError,
+    ) as e:
+        sys.exit(e)
--- koozie/koozie.py
+++ koozie/koozie.py
@@ -1,14 +1,25 @@
+""" The koozie module supplies conversion wrapper functions for the pint module."""
+
+from collections import OrderedDict
 import importlib.resources as pkg_resources
 import pint
-from collections import OrderedDict
 
 # Edit constants template to stop using h to represent planck_constant
 # See https://github.com/hgrecco/pint/issues/719#issuecomment-998872301
 # ---------------------------------------------------------------------
-constants_template = pkg_resources.read_text(pint, 'constants_en.txt').replace("= h  ", "     ").replace(" h ", " planck_constant ")
+constants_template = (
+    pkg_resources.read_text(pint, "constants_en.txt")
+    .replace("= h  ", "     ")
+    .replace(" h ", " planck_constant ")
+)
 
 # Edit units template to use h to represent hour instead of planck_constant
-units_template = pkg_resources.read_text(pint, 'default_en.txt').replace("@import constants_en.txt", "").replace(" h ", " planck_constant ").replace("hour = 60 * minute = hr", "hour = 60 * minute = h = hr")
+units_template = (
+    pkg_resources.read_text(pint, "default_en.txt")
+    .replace("@import constants_en.txt", "")
+    .replace(" h ", " planck_constant ")
+    .replace("hour = 60 * minute = hr", "hour = 60 * minute = h = hr")
+)
 
 # Join templates as iterable object
 full_template = constants_template.split("\n") + units_template.split("\n")
@@ -19,85 +30,86 @@ ureg.default_format = "~P"  # short pretty
 # ---------------------------------------------------------------------
 
 # Add new aliases
-ureg.define('@alias inch_H2O_39F = in_H2O')
-ureg.define('@alias ton_of_refrigeration = ton_ref')
+ureg.define("@alias inch_H2O_39F = in_H2O")
+ureg.define("@alias ton_of_refrigeration = ton_ref")
 
 # Add new derived dimensions
-ureg.define('[thermal_resistance] = [area] * [temperature] / [power]')
-ureg.define('[thermal_conductance] = [power] / ([area] * [temperature])')
-ureg.define('[volumetric_flow_rate] = [length] ** 3 / [time]')
+ureg.define("[thermal_resistance] = [area] * [temperature] / [power]")
+ureg.define("[thermal_conductance] = [power] / ([area] * [temperature])")
+ureg.define("[volumetric_flow_rate] = [length] ** 3 / [time]")
 
 # Add new units
-ureg.define('cubic_feet_per_minute = cu_ft / min = cfm')
-ureg.define('gallons_per_minute = gallon / min = gpm')
-ureg.define('thermal_resistance_SI = m**2*K/W = R_value_SI = R_SI')
-ureg.define('thermal_resistance_IP = ft**2*degR*h/Btu = R_value_IP = R_IP')
-ureg.define('thermal_conductance_SI = W/(m**2*K) = U_factor_SI = U_SI')
-ureg.define('thermal_conductance_IP = Btu/(ft**2*degR*h) = U_factor_IP = U_IP')
+ureg.define("cubic_feet_per_minute = cu_ft / min = cfm")
+ureg.define("gallons_per_minute = gallon / min = gpm")
+ureg.define("thermal_resistance_SI = m**2*K/W = R_value_SI = R_SI")
+ureg.define("thermal_resistance_IP = ft**2*degR*h/Btu = R_value_IP = R_IP")
+ureg.define("thermal_conductance_SI = W/(m**2*K) = U_factor_SI = U_SI")
+ureg.define("thermal_conductance_IP = Btu/(ft**2*degR*h) = U_factor_IP = U_IP")
+
 
 # Private functions (used in CLI)
 def fr_q(value, from_units):
-  '''Convert a value from given units to a quantity in base SI units'''
-  return ureg.Quantity(value, from_units).to_base_units()
+    """Convert a value from given units to a quantity in base SI units"""
+    return ureg.Quantity(value, from_units).to_base_units()
+
 
 def to_q(value, to_units):
-  '''Convert a value from base SI units to a quantity in any other units'''
-  base_units = ureg.Quantity(value, to_units).to_base_units().units
-  return ureg.Quantity(value, base_units).to(to_units)
+    """Convert a value from base SI units to a quantity in any other units"""
+    base_units = ureg.Quantity(value, to_units).to_base_units().units
+    return ureg.Quantity(value, base_units).to(to_units)
+
 
 def convert_q(value, from_units, to_units):
-  '''Convert a value from any units to a quantity in another units of the same dimension'''
-  return ureg.Quantity(value, from_units).to(to_units)
+    """Convert a value from any units to a quantity in another units of the same dimension"""
+    return ureg.Quantity(value, from_units).to(to_units)
+
 
 # Public functions
 def fr_u(value, from_units):
-  '''Convert a value from given units to base SI units'''
-  return fr_q(value, from_units).magnitude
+    """Convert a value from given units to base SI units"""
+    return fr_q(value, from_units).magnitude
+
 
 def to_u(value, to_units):
-  '''Convert a value from base SI units to any other units'''
-  return to_q(value, to_units).magnitude
+    """Convert a value from base SI units to any other units"""
+    return to_q(value, to_units).magnitude
+
 
 def convert(value, from_units, to_units):
-  '''Convert a value from any units to another units of the same dimension'''
-  return convert_q(value, from_units, to_units).magnitude
+    """Convert a value from any units to another units of the same dimension"""
+    return convert_q(value, from_units, to_units).magnitude
 
-def sorting_function(x):
-  return len(x[0])
+
+def sorting_function(x):  # pylint: disable=C0103
+    """For OrderedDict of units"""
+    return len(x[0])
 
 
 def get_unit_list():
-  unit_list = {}
-  for u in ureg:
-    if u in ureg: # Certain symbols do not show up
-      if ureg[u].dimensionless:
-        dimensionality = "[]"
-      else:
-        dimensionality = f"{ureg[u].dimensionality}"#.replace('[','').replace(']','').replace(' ** ','^')
-      if dimensionality not in unit_list:
-        unit_list[dimensionality] = {
-          "aliases": [],
-          "units": {}
-        }
-      unit = list(ureg[u]._units._d.keys())[0]
-      if unit not in unit_list[dimensionality]["units"]:
-        unit_list[dimensionality]["units"][unit] = []
-      if u != unit:
-        unit_list[dimensionality]["units"][unit].append(u)
+    """Return an OrderedDict of units sorted by length"""
+    unit_list = {}
+    for u in ureg:  # pylint: disable=C0103
+        if u in ureg:  # Certain symbols do not show up
+            if ureg[u].dimensionless:
+                dimensionality = "[]"
+            else:
+                dimensionality = f"{ureg[u].dimensionality}"  # .replace('[','').replace(']','').replace(' ** ','^')
+            if dimensionality not in unit_list:
+                unit_list[dimensionality] = {"aliases": [], "units": {}}
+            unit = list(ureg[u]._units._d.keys())[0]  # pylint: disable=W0212
+            if unit not in unit_list[dimensionality]["units"]:
+                unit_list[dimensionality]["units"][unit] = []
+            if u != unit:
+                unit_list[dimensionality]["units"][unit].append(u)
 
-  # Get dimension aliases
-  for dim in ureg._dimensions:
-    if dim in unit_list:
-      continue
-    else:
-      dimensionality = f"{ureg.get_dimensionality(ureg._dimensions[dim].reference)}"
-      #if dimensionality == "None":
-      #  dimensionality = "[]"
-      if dimensionality not in unit_list:
-        unit_list[dimensionality] = {
-          "aliases": [],
-          "units": {}
-        }
-      unit_list[dimensionality]["aliases"].append(dim)
-  return OrderedDict(sorted(unit_list.items(), key=sorting_function))
-
+    # Get dimension aliases
+    for dim in ureg._dimensions:  # pylint: disable=W0212
+        if dim in unit_list:
+            continue
+        dimensionality = f"{ureg.get_dimensionality(ureg._dimensions[dim].reference)}"  # pylint: disable=W0212
+        # if dimensionality == "None":
+        #  dimensionality = "[]"
+        if dimensionality not in unit_list:
+            unit_list[dimensionality] = {"aliases": [], "units": {}}
+        unit_list[dimensionality]["aliases"].append(dim)
+    return OrderedDict(sorted(unit_list.items(), key=sorting_function))
