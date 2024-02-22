9322c9dd02f5106452932a779b9572f36feb306e
Neal Kruis <neal.kruis@bigladdersoftware.com>
2024-02-22 15:52:04 -0700

    Fix issue when passing integer values.

--- test/test_koozie.py
+++ test/test_koozie.py
@@ -1,4 +1,5 @@
 """koozie unit tests"""
+
 from pytest import approx
 from click.testing import CliRunner
 from koozie import fr_u, to_u, convert, get_dimensionality
@@ -12,6 +13,7 @@ def test_units():
     assert fr_u(32.0, "°F") == approx(fr_u(0.0, "°C"))
     assert to_u(273.15, "°C") == approx(0.0)
     assert fr_u(1.0, "in") == approx(0.0254)
+    assert fr_u(1, "in") == approx(0.0254)
     assert to_u(0.0254, "in") == approx(1.0)
     assert fr_u(3.41241633, "Btu/h") == approx(1.0, 0.0001)
 
