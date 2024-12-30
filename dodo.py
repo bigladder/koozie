8b07ca2eca18af8e917008d59f3f8da3aa2724c0
Neal Kruis <neal.kruis@bigladdersoftware.com>
2024-12-30 11:52:34 -0700

    Apply workarounds for weird Windows issues.

--- dodo.py
+++ dodo.py
@@ -8,13 +8,17 @@ project_name = "koozie"
 def task_check_code():
     """Run linter(s)"""
     checkers = ["pylint", "mypy", "black --check"]
-    source_checking_paths = [project_name, "test/*.py"]
+    source_checking_paths = [project_name, "test"]
 
     for checker in checkers:
         for path in source_checking_paths:
+            command = f"{checker} {path}"
+            if checker == "pylint":
+                # Get around weird issue on Windows
+                command += "/*.py"
             yield {
                 "name": f"{checker} {path}",
-                "actions": [f"{checker} {path}"],
+                "actions": [command],
                 "verbosity": 1,  # print only errors
             }
 
